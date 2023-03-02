469dd02ba7b77dbc019d59a41077815d1ba0e257
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-03-02 19:43:32 +0200

    second-one

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { IAppProps } from './IApp.interfase';
+
+
+function App({...props}: IAppProps): JSX.Element {
+  return <div className="App"> 
+    
+    </div>;
+}
+
+export default App;
---
+++ src/components/App/IApp.interfase.tsx
@@ -0,0 +1,3 @@
+export interface IAppProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    name : string
+  }
