797349d51a8b0ea3d95f0b39fbfb0e768b97f9c6
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-03-04 14:07:55 +0200

    i deleted everything

--- templates/default/TemplateName.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './TemplateName.module.scss';
-import { TemplateNameProps } from './TemplateName.interface';
-
-
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.templateName} >
-    TemplateName Component
-  </div>
-);
