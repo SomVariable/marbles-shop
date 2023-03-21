931009cd308fe9a2fa72ed53e8cd8577112a5bd8
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-03-21 10:27:11 +0200

    safe commit

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -1,9 +1,13 @@
+import Button from "../../UI/Button/Button"
 import styles from "./styles/Example.module.scss"
 interface ExampleProps {
   
 }
 const Example = ({}: ExampleProps) => (
-  <div>Example 2</div>
-
+  <div>Example 2
+    <Button appearance = "primary" isOption = {true} >Католог</Button>
+    <Button  appearance = "secondary">В корзину</Button>
+    <Button  appearance = "primary">Доавить в корзину</Button>
+  </div>
 )
 export default Example
