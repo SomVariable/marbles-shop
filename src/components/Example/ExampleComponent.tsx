8b46c80b8e2de05a8dc0ad0f924a6aa6d43825a8
SomVariable <79557545+SomVariable@users.noreply.github.com>
2023-03-25 11:35:04 +0200

    finished icon module

--- src/components/Example/ExampleComponent.tsx
+++ src/components/Example/ExampleComponent.tsx
@@ -1,19 +1,17 @@
 import Button from "../../UI/Button/Button"
+import CustomIcon from "../../UI/Icon/Icon"
 import styles from "./styles/Example.module.scss"
 interface ExampleProps {
   
 }
 const ExampleComponent = ({}: ExampleProps) => (
   <div>
-    <Button appearance = "option" isOption = {true} >Католог</Button>
-    <Button  appearance = "primary">В корзину</Button>
-    <Button  appearance = "secondary">Доавить в корзину</Button>
-    <div style = {{
-      backgroundColor: 'black',
-      padding: "10px",
-      margin: "20px"
-    }}><Button  appearance = "shadow">Получить скидку</Button>
-    </div>
+
+    <CustomIcon type="cart"/>
+    <CustomIcon type="instagram"/>
+    <CustomIcon type="search"/>
+    <CustomIcon type="shit"/>
+
   </div>
 
 )
