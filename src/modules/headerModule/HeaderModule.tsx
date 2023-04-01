17773930a2e9c6e098aee95cea49327f1661ad10
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-04-01 18:10:23 +0300

    add subHeader

--- src/modules/headerModule/HeaderModule.tsx
+++ src/modules/headerModule/HeaderModule.tsx
@@ -1,11 +1,13 @@
 import styles from './HeaderModule.module.scss';
 import Header from '../../components/header/Header';
+import SubHeader from '../../components/subHeader/SubHeader';
 
 const HeaderModule = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Header/>
+        <SubHeader/>
       </div>
     </header>
   )
